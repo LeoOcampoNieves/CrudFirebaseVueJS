e9f76a8f0487fefc5225de8880203a08f9c15800
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-04-15 04:19:46 -0500

    Subida de archivos

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDrxGPFkoAxsVywar2Xt4UIsf5dX0vIgto",
+    authDomain: "crudfirebasevuejs.firebaseapp.com",
+    databaseURL: "https://crudfirebasevuejs.firebaseio.com",
+    projectId: "crudfirebasevuejs",
+    storageBucket: "crudfirebasevuejs.appspot.com",
+    messagingSenderId: "558578021476",
+    appId: "1:558578021476:web:7789913ed8da33150e1b71"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
